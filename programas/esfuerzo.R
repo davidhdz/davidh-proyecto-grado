02c2892422eaf1db636bcb240fbde08429991fba
David Hernández <david.vzla@gmail.com>
2017-04-06 04:17:12 -0400

    Se agregan los cálculos de COCOMO para Ubuntu

--- programas/esfuerzo.R
+++ programas/esfuerzo.R
@@ -1,13 +1,20 @@
 #! /usr/bin/env Rscript
+library(tcltk)
 
-cloc <- read.csv("../datos/cloc.csv", header=TRUE)
+#Filtro de extensiones con las que se trabajará en este script
+Filters   <- matrix(c("csv",".csv"),1, 2, byrow = TRUE)
+
+#Cargar el archivo de entrada
+entrada <- tk_choose.files(default = "", caption = "Seleccione el archivo de entrada",multi = FALSE, filters = Filters, index = 1)
+
+cloc <- read.csv(entrada, header=TRUE)
 
 a <- 2.40
 b <- 1.05
 c <- 2.5
 d <- 0.38
-KSLOC <- sum(cloc$Codigo)/1000
-Spa <- 60445
+KSLOC <- sum(cloc$code)/1000
+Spa <- 73837
 Epm <- a*(KSLOC)^b
 Prod <- KSLOC/Epm
 Tdev <- c*(Epm)^d
@@ -15,13 +22,14 @@ Per <- Epm/Tdev
 Ctd <- Per*Spa
 
 cat("\n\n")
-cat("Líneas de código fuente:", sum(cloc$Codigo),"\n\n\n")
+cat("Líneas de código fuente: \t", format(sum(cloc$code), digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n")
+cat("Líneas de código fuente (K): \t", format(KSLOC, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n\n\n")
 cat("Estimaciones aplicando el Modelo COCOMO Básico\n")
-cat("==========================================================\n")
-cat("Esfuerzo estimado de desarrollo:\t\t", Epm,"\n")
-cat("Productividad estimada:\t\t\t\t", Prod,"\n")
-cat("Tiempo de desarrollo estimado:\t\t\t", Tdev,"\n")
-cat("Personas requeridas estimadas:\t\t\t", Per,"\n")
-cat("\nCosto total estimado del proyecto(*):\t\t", Ctd, "\n\n")
-cat("(*) con US$", Spa, "anual como referencia para enero de 2017\n")
-cat("==========================================================\n\n\n")
+cat("==============================================================\n")
+cat("Esfuerzo estimado de desarrollo:\t", format(Epm, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n")
+cat("Productividad estimada:\t\t\t", format(Prod, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n")
+cat("Tiempo de desarrollo estimado:\t\t", format(Tdev, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n")
+cat("Personas requeridas estimadas:\t\t", format(Per, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE),"\n")
+cat("\nCosto total estimado del proyecto(*):\t", format(Ctd, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE), "\n\n")
+cat("(*) con US$", format(Spa, digits=2, nsmall=2, decimal.mark=",", big.mark=".", scientific=FALSE), "anual como referencia para enero de 2017\n")
+cat("==============================================================\n\n\n")
--- programas/estadisticas.R
+++ programas/estadisticas.R
@@ -1,14 +1,42 @@
 #! /usr/bin/env Rscript
+library(tcltk)
 
-png('../imagenes/rplot.png', width = 800)
-cloc <- read.csv("../datos/cloc.csv", header=TRUE)
-barplot(as.matrix(cloc[,3:5]),beside = TRUE,legend=(cloc$Lenguaje), args.legend = list(bty="n", horiz=FALSE), border="white",ylab = "Líneas de código", main = "Cantidad de líneas de código, comentarios y líneas vacías por Lenguaje", col=topo.colors(length(cloc$Lenguaje)))
+#Filtro de extensiones con las que se trabajará en este script
+Filters   <- matrix(c("csv",".csv"),1, 2, byrow = TRUE)
+
+#Cargar el archivo de entrada
+entrada <- tk_choose.files(default = "", caption = "Seleccione el archivo de entrada",multi = FALSE, filters = Filters, index = 1)
+
+ahora <- Sys.time()
+tiempo <- strftime(ahora,"%Y%m%d%H%M%S")
+png(sprintf('./plot_%s.png', tiempo), width = 800)
+
+cloc <- as.data.frame(read.csv(entrada, header=TRUE))
+#barplot(as.matrix(cloc[,3:5]),beside = TRUE, horiz=FALSE,legend=(cloc$language), args.legend = list(bty="n", horiz=FALSE), border="white",ylab = "Líneas de código", main = "Cantidad de líneas de código, comentarios y líneas vacías por Lenguaje", col=topo.colors(length(cloc$language)))
+#grid(nx=NA)
+porcentaje <- round(100*cloc$code/sum(cloc$code), 1)
+porcentaje
+#etiquetas <- paste(porcentaje, "%", sep="")
+#pie(cloc$code, main="Lenguajes", col=topo.colors(length(cloc$language)), labels=etiquetas)
+#legend("topleft", c(cloc$language), fill=topo.colors(length(cloc$language)))
+#plot(porcentaje,cloc$code)
+#hist(cloc$code)
+#dotchart(cloc[,5], labels=cloc$language, cex = 0.3, pch = 16, main = "Líneas de código por lenguaje", cex.lab=0.5)
+ss<-subset(cloc,cloc$code>1230000)
+ss[,1:5]
+cloc[,1:5]
+#qplot(cloc$code, cloc$files, data = cloc, xlab="Número de líneas de código", ylab = "Cantidad de archivos", size=(cloc$code/cloc$files), color=I("blue"), alpha=I(0.7))
+#plot(cloc$files,cloc$code,main = "Relación entre Líneas de código por lenguaje y cantidad de archivos por lenguaje",pch=19, col="blue", xlab="Número de archivos", ylab="Número de líneas de código")
+#barplot(cloc$code, names.arg = cloc$language, col=topo.colors(length(cloc$language)), horiz = FALSE, border = "white")
+
+
 dev.off()
 
 cat("\n\n")
 cat("==========================================================\n")
-cat("Total archivos con lenguajes reconocidos:\t", sum(cloc$Archivos),"\n")
-cat("Total de líneas en blanco:\t\t\t", sum(cloc$En_blanco),"\n")
-cat("Total de líneas con comentarios:\t\t", sum(cloc$Comentarios),"\n")
-cat("\nTotal de líneas de código fuente:\t\t", sum(cloc$Codigo),"\n")
+cat("Total archivos con lenguajes reconocidos:\t", sum(cloc$files),"\n")
+cat("Total de líneas en blanco:\t\t\t", sum(cloc$blank),"\n")
+cat("Total de líneas con comentarios:\t\t", sum(cloc$comment),"\n")
+cat("\nTotal de líneas de código fuente:\t\t", sum(cloc$code),"\n")
 cat("==========================================================\n\n\n")
+
